1b6bcce83d1ac01e7781232e52d3749938d38af3
gustavo-garcia-de-sousa <sousagustavogarcia@gmail.com>
2023-06-02 09:25:36 -0300

    aprendendo a utilizar: hook useSate

--- src/components/Forms/Forms.js
+++ src/components/Forms/Forms.js
@@ -2,20 +2,43 @@ import './Forms.css';
 import TextField from '../TextField/TextField';
 import Selected from '../Selected/Selected';
 import Buttons from '../Button/Button';
-
-function saving(event) {
-    event.preventDefault();//previne o comportamento padrão do formulário
-    console.log('formulário submetido');
-}
+import { useState } from 'react';
 
 const Forms = () => {
+
+    const [nome, setNome] = useState('')//useState é um hook que permite que o componente tenha estado
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+
+    const aoSalvar = (event) => {
+        event.preventDefault();//previne o comportamento padrão do formulário
+        console.log('formulário submetido =>', nome, cargo, imagem);
+    }
+
     return (
         <section className='forms'>
-            <form onSubmit={() => saving()}> {/*onSubmit é um evento que é disparado quando o formulário é submetido*/}
+            <form onSubmit={aoSalvar}> {/*onSubmit é um evento que é disparado quando o formulário é submetido*/}
                 <h2>Formulário de Cadastro</h2>
-                <TextField required={true} label="Nome" placeholder="Digite seu nome completo" />
-                <TextField required={true} label="Cargo" placeholder="Digite seu cargo" />
-                <TextField required={true} label="Imagem" placeholder="Insira o caminho da imagem" />
+                <TextField
+                    required={true}
+                    label="Nome"
+                    placeholder="Digite seu nome completo"
+                    value={nome}
+                    aoAlterar={value => setNome(value)}
+                />
+                <TextField
+                    required={true}
+                    label="Cargo"
+                    placeholder="Digite seu cargo"
+                    value={cargo}
+                    aoAlterar={value => setCargo(value)}
+                />
+                <TextField
+                    label="Imagem"
+                    placeholder="Insira o caminho da imagem"
+                    value={imagem}
+                    aoAlterar={value => setImagem(value)}
+                />
                 <Selected label="País" itens={['Brasil', 'Argentina', 'Chile', 'Uruguai', 'Paraguai']} />
                 <Buttons> Criar Card </Buttons>
             </form>
--- src/components/TextField/TextField.js
+++ src/components/TextField/TextField.js
@@ -1,11 +1,30 @@
 import './TextField.css';
 
 const TextField = (props) => {//props são as propriedades que são passadas para o componente
+
+    //let value = '';
+
+    //const [value, setValue] = useState('');//useState é um hook que permite que o componente tenha estado
+
+    const aoDigitar = (event) => {//função que é disparada quando o usuário digita no input
+        //value = event.target.value;
+        //setValue(event.target.value);//alterando o valor da variável value
+        //console.log(value);
+
+        props.aoAlterar(event.target.value);//disparando a função que foi passada como propriedade          
+
+    }
+
     return (//acessando o label e o placeholder que foi passado como propriedade
-        <div className='text-field'>
+        <div className='text-field' >
             <label>{props.label}</label>
-            <input required={props.required} type="text" placeholder={props.placeholder} />
-        </div>
+            <input
+                value={props.value}
+                onChange={aoDigitar}//onChange é um evento que é disparado quando o usuário digita no input
+                required={props.required}
+                placeholder={props.placeholder}
+            />
+        </div >
     );
 }
 
